6fc6a2bb70c85dfd8149d86422497d4ecc7cc08c
John Daniels Adeyemi <adeyemijohndaniels@gmail.com>
2022-10-20 17:52:53 +0100

    added a smooth animation to the splashscreen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:tech_tutor/home.dart';
-
+import 'package:tech_tutor/Student/signupstudent.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -11,10 +10,12 @@ class SplashScreen extends StatelessWidget {
     return AnimatedSplashScreen(
       splash: Image.asset("assets/images/logo.png"),
       backgroundColor: Colors.white,
-      nextScreen: const HomeScreen(),
+      nextScreen: const SignUpStudent(),
       duration: 4000,
       splashTransition: SplashTransition.rotationTransition,
       splashIconSize: 250,
+      animationDuration: const Duration(milliseconds: 800),
+      curve: Curves.easeInOut,
     );
   }
 }
